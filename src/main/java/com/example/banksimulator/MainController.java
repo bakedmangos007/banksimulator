cde69cd8b1346cff3cb6521555194a47d29a7c8e
Ben Vail <benjyvail@icloud.com>
2023-12-20 16:21:10 +0800

    Added Commit

--- src/main/java/com/example/banksimulator/MainController.java
+++ src/main/java/com/example/banksimulator/MainController.java
@@ -4,14 +4,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
 
 @Controller
 public class MainController {
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
 
-    @GetMapping("/test")
-    public String getTest() {
-        return "test";
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "registration";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationForm(User user) {
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
     }
 }
--- src/main/java/com/example/banksimulator/User.java
+++ src/main/java/com/example/banksimulator/User.java
@@ -1,43 +1,36 @@
 package com.example.banksimulator;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-
-
-@Entity // This tells Hibernate to make a table out of this class
+@Entity
 public class User {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    private String name;
+    @Column(unique = true)
+    private String username;
 
-    private String email;
+    private String password;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public String getPassword() {
+        return password;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
